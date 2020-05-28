584b9f0caf192b65b0527e0d6575561619de213d
Julia Margarita Jimenez Herrera <maggie.jh99@gmail.com>
2020-05-28 04:44:39 -0500

    Cambiando config

--- scr/config.js
+++ scr/config.js
@@ -1,5 +1,5 @@
 
 exports.PORT = process.env.PORT || '8080';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb+srv://admi:holyBrownies@holybrownies-drkuv.mongodb.net/Holybrowniesdb?retryWrites=true&w=majority'
+exports.DATABASE_URL = process.env.DATABASE_URL;
 exports.TOKEN_ADMI = process.env.TOKEN_ADMI || 'TokenAdmi';
 exports.SECRET_TOKEN = process.env.SECRET_TOKEN || 'secret'
